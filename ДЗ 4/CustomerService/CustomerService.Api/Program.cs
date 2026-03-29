c4441025639b51392cc036338ee30b917bcb18e6
1Mapa1 <maslovpavel333@gmail.com>
2026-03-29 16:37:21 +0300

    add fix to programa

--- ДЗ 4/CustomerService/CustomerService.Api/Program.cs
+++ ДЗ 4/CustomerService/CustomerService.Api/Program.cs
@@ -1,6 +1,7 @@
 using CustomerService.Api.Mapping;
 using CustomerService.Infrastructure;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Prometheus;
 
 namespace CustomerService
 {
@@ -15,9 +16,35 @@ namespace CustomerService
             builder.Services.AddSwaggerGen();
             builder.Services.AddAutoMapper(_ => { }, typeof(MappingProfile));
             builder.Services.AddInfrastructure(builder.Configuration);
-            builder.Services.AddInfrastructureHealthChecks();
+            builder.Services
+                .AddInfrastructureHealthChecks()
+                .ForwardToPrometheus();
 
             var app = builder.Build();
+
+            app.UseHttpMetrics(options =>
+            {
+                options.RequestDuration.Histogram = Metrics.CreateHistogram(
+                    "http_request_duration_seconds", 
+                    "Duration of HTTP requests in seconds",
+                    new HistogramConfiguration
+                    {
+                        Buckets =
+                        [
+                            0.001,   // 1 ms
+                            0.0025,  // 2.5 ms
+                            0.005,   // 5 ms
+                            0.0075,  // 7.5 ms
+                            0.01,    // 10 ms
+                            0.025,   // 25 ms
+                            0.05,    // 50 ms
+                            0.1,     // 100 ms
+                            0.25,    // 250 ms
+                            0.5,     // 500 ms
+                            1.0      // 1 s
+                        ]
+                    });
+            });
 
             if (app.Environment.IsDevelopment())
             {
@@ -41,6 +68,8 @@ namespace CustomerService
             {
                 Predicate = check => check.Tags.Contains("startup")
             });
+
+            app.MapMetrics();
 
             app.Run();
         }
